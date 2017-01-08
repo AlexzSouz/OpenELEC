faf7950878f00a90f3765b5344ce965c30ab86bf
higtrollers <alexandre.higtrollers@gmail.com>
2017-01-08 16:50:48 +0000

    OpenELEC initial code

---
+++ Addons/Netflix/plugin/video/netflix/netflix.py
@@ -0,0 +1,7 @@
+# http://openelec.tv
+
+# http://kodi.wiki/view/Add-on_development
+# http://kodi.wiki/view/Add-on_structure
+# http://kodi.wiki/view/Addon.xml
+
+# https://en.wikipedia.org/wiki/Python_syntax_and_semantics
